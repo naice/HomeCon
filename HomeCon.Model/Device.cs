f072513a26e365695df099d8c0bb1580923f2d03
Jens Marchewka <im.naice@googlemail.com>
2017-12-18 21:31:28 +0100

    ADD | Added shared Models (Alexa models)

---
+++ HomeCon.Model/Device.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeCon.Model
+{
+    public class Device
+    {
+        /// <summary>
+        /// A device identifier. The identifier must be unique across all devices owned by an end user within the domain for the skill. In addition, the identifier needs to be consistent across multiple discovery requests for the same device. An identifier can contain any letter or number and the following special characters: _ - = # ; : ? @ &. The identifier cannot exceed 256 characters.	String that contains any letter or number and the following special characters: _ - = # ; : ? @ 
+        /// </summary>
+        public string EndPointId { get; set; }
+        /// <summary>
+        /// The name of the device manufacturer. This value cannot exceed 128 characters.
+        /// </summary>
+        public string ManufracturerName { get; set; }
+        /// <summary>
+        /// The name used by the customer to identify the device. This value cannot exceed 128 characters and should not contain special characters or punctuation.	
+        /// </summary>
+        public string FriendlyName { get; set; }
+        /// <summary>
+        /// A human-readable description of the device. This value cannot exceed 128 characters. The description should contain the manufacturer name or how the device is connected. For example, "Smart Lock by Sample Manufacturer" or "WiFi Thermostat connected via SmartHub". This value cannot exceed 128 characters.
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Indicates the group name where the device should display in the Alexa app. See Display categories for supported values.
+        /// </summary>
+        public List<string> DisplayCategories { get; set; }
+        /// <summary>
+        /// String name/value pairs that provide additional information about a device for use by the skill. The contents of this property cannot exceed 5000 bytes. The API doesn't use or understand this data.
+        /// </summary>
+        public Dictionary<string,string> Cookie { get; set; }
+        /// <summary>
+        /// An array of capability objects that represents actions particular device supports and can respond to. A capability object can contain different fields depending on the type.
+        /// </summary>
+        public List<string> Capabilities { get; set; }
+    }
+}
---
+++ HomeCon.Model/DisplayCategories.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeCon.Model
+{
+    public abstract class DisplayCategories
+    {
+        ///<summary>
+        /// Describes a combination of devices set to a specific state, when the state change must occur in a specific order.For example, a “watch Neflix” scene might require the: 1. TV to be powered on & 2. Input set to HDMI1.Applies to Scenes
+        ///</summary>
+        public const string ACTIVITY_TRIGGER = "ACTIVITY_TRIGGER";
+        ///<summary>
+        /// Indicates media devices with video or photo capabilities.
+        ///</summary>
+        public const string CAMERA = "CAMERA";
+        ///<summary>
+        /// Indicates a door.	 
+        ///</summary>
+        public const string DOOR = "DOOR";
+        ///<summary>
+        /// LIGHT
+        ///</summary>
+        public const string LIGHT = "LIGHT";
+        ///<summary>
+        /// OTHER
+        ///</summary>
+        public const string OTHER = "OTHER";
+        ///<summary>
+        /// Describes a combination of devices set to a specific state, when the order of the state change is not important. For example a bedtime scene might include turning off lights and lowering the thermostat, but the order is unimportant.Applies to Scenes
+        ///</summary>
+        public const string SCENE_TRIGGER = "SCENE_TRIGGER";
+        ///<summary>
+        /// Indicates an endpoint that locks.
+        ///</summary>
+        public const string SMARTLOCK = "SMARTLOCK  ";
+        ///<summary>
+        /// Indicates modules that are plugged into an existing electrical outlet.Can control a variety of devices.
+        ///</summary>
+        public const string SMARTPLUG = "SMARTPLUG";
+        ///<summary>
+        /// Indicates the endpoint is a speaker or speaker system.
+        ///</summary>
+        public const string SPEAKER = "SPEAKER";
+        ///<summary>
+        /// Indicates in-wall switches wired to the electrical system.Can control a variety of devices.
+        ///</summary>
+        public const string SWITCH = "SWITCH";
+        ///<summary>
+        /// Indicates endpoints that report the temperature only.	 
+        ///</summary>
+        public const string TEMPERATURE_SENSOR = "TEMPERATURE_SENSOR";
+        ///<summary>
+        /// Indicates endpoints that control temperature, stand-alone air conditioners, or heaters with direct temperature control.	 
+        ///</summary>
+        public const string THERMOSTAT = "THERMOSTAT";
+        ///<summary>
+        /// Indicates the endpoint is a television.
+        ///</summary>
+        public const string TV = "TV";
+
+        /// <summary>
+        /// All possible DisplayCategories.
+        /// </summary>
+        public static readonly IReadOnlyList<string> All = new List<string>()
+        {
+            ACTIVITY_TRIGGER, CAMERA, DOOR, LIGHT, OTHER, SCENE_TRIGGER, SMARTLOCK,
+            SMARTPLUG, SPEAKER, SWITCH, TEMPERATURE_SENSOR, THERMOSTAT, TV,
+        };
+    }
+}
